0755381ff6e7bc0deecf6d98e2ffcd7720d87c0a
montysingh <montysingh9527@gmail.com>
2021-12-27 11:32:02 +0800

    init toutiao

---
+++ src/api/user.js
@@ -0,0 +1,12 @@
+import request from '@/utils/request'
+// 使用 Vuex 必须先加载
+// import store from '@/store'
+
+// 登录
+export const login = data => {
+  return request({
+    method: 'POST',
+    url: '/app/v1_0/authorizations',
+    data
+  })
+}
---
+++ src/utils/request.js
@@ -0,0 +1,11 @@
+import axios from 'axios' // 使用 Vuex 必须先加载
+
+
+// 再次创建一个实例 防止加载之前的会走拦截器
+// 用来回去新的 token
+const request  = axios.create({
+  baseURL: 'http://toutiao-app.itheima.net'
+})
+
+// 导出
+export default request
