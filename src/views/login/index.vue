0145701b977b241a0f552c5ccecca7106eba8d1c
montysingh <montysingh9527@gmail.com>
2021-12-29 10:54:57 +0800

    实现基本登录功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,10 +2,21 @@
   <div class="login-container">
     <van-nav-bar class="page-nav-bar" title="登录" />
     <van-form @submit="onSubmit">
-      <van-field name="手机号" placeholder="请输入手机号" left-icon="smile-o">
+      <van-field
+        v-model="user.mobile"
+        name="手机号"
+        placeholder="请输入手机号"
+        left-icon="smile-o"
+        :rules="[{ required: false }]"
+      >
         <i slot="left-icon" class="toutiao toutiao-shouji"></i>
       </van-field>
-      <van-field name="验证码" placeholder="请输入验证码">
+      <van-field
+        v-model="user.code"
+        name="验证码"
+        placeholder="请输入验证码"
+        :rules="[{ required: false }]"
+      >
         <i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
         <template #button>
           <van-button class="send-sms-code" round size="small" type="default"
@@ -14,25 +25,49 @@
         </template>
       </van-field>
       <div class="login-btn-wrap">
-        <van-button class="login-btn" block type="info" native-type="submit">登录</van-button>
+        <van-button class="login-btn" block type="info" native-type="submit"
+          >登录</van-button
+        >
       </div>
     </van-form>
   </div>
 </template>
 
 <script>
+import { login } from '@/api/user';
 export default {
-  name: "LoginIndex",
+  name: 'LoginIndex',
   components: {},
   props: {},
   data() {
-    return {};
+    return {
+      user: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+      },
+    };
   },
   computed: {},
   watch: {},
   created() {},
   methods: {
-    onSubmit() {},
+    async onSubmit() {
+      // 1、获取表单数据
+      const user = this.user;
+      // 2、表单验证
+      // 3、提交表单请求登录
+      try {
+        const res = await login(user);
+        console.log("登录成功", res);
+      } catch (err) {
+        if (err.response.status === 400) {
+          console.log("手机号或验证码错误");
+        } else {
+          console.log("登录失败", err);
+        }
+      }
+      // 4、根据请求响应结果处理后续操作
+    },
   },
 };
 </script>
@@ -50,12 +85,12 @@ export default {
     color: #666;
     background-color: #ededed;
   }
-  .login-btn-wrap{
-      padding: 53px 33px ;
-      .login-btn{
-          background-color: #6db4fb;
-          border: none;
-      }
+  .login-btn-wrap {
+    padding: 53px 33px;
+    .login-btn {
+      background-color: #6db4fb;
+      border: none;
+    }
   }
 }
 </style>
