44001dcce704b8412fb16262cd962d935d693a11
montysingh <montysingh9527@gmail.com>
2021-12-31 01:29:53 +0800

    TabBar创建底部菜单栏 首页、问答、视频、我的,路由配置

---
+++ src/views/layout/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="layout-container">
+    <!-- 子路由出口 -->
+    <router-view />
+    <!-- / 子路由出口 -->
+    <!-- 标签导航栏 roout:开启路由模式 -->
+    <van-tabbar class="layout-tabbar" route>
+      <van-tabbar-item to="/">
+        <i slot="icon" class="toutiao toutiao-shouye" />
+        <span class="text">首页</span>
+      </van-tabbar-item>
+      <van-tabbar-item to="/qa">
+        <i slot="icon" class="toutiao toutiao-wenda" />
+        <span class="text">问答</span>
+      </van-tabbar-item>
+      <van-tabbar-item to="/video">
+        <i slot="icon" class="toutiao toutiao-shipin" />
+        <span class="text">视频</span>
+      </van-tabbar-item>
+      <van-tabbar-item to="/my">
+        <i slot="icon" class="toutiao toutiao-wode" />
+        <span class="text">我的</span>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LayoutIndex",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="less">
+.layout-container {
+  .layout-tabbar {
+    i.toutiao {
+      font-size: 40px;
+    }
+    span.text {
+      font-size: 20px;
+    }
+  }
+}
+</style>
---
+++ src/views/video/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="video-container">视频</div>
+</template>
+
+<script>
+export default {
+  name: "VideoIndex",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="less">
+</style>
