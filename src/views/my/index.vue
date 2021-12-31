7d0d670cf5013662d16e56d1aa2f97e2b7fa6083
montysingh <montysingh9527@gmail.com>
2022-01-01 01:17:51 +0800

    我的-已登录页面布局完成

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -5,8 +5,53 @@
         <img class="mobile-img" src="~@/assets/mobile.png" alt="">
         <span class="text">登录|注册</span>
       </div>
-
     </div>
+
+    <!-- 已登录状态布局 -->
+    <div class="header user-info">
+      <div class="base-info">
+        <div class="left">
+            <van-image class="avatar" src="https://img01.yzcdn.cn/vant/cat.jpeg" fit="cover" round />
+            <span class="name">今日头条号</span>
+        </div>
+        <div class="right">
+          <van-button size="mini" round>编辑资料</van-button>
+        </div>
+      </div>
+      <div class="data-stats">
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">头条</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">关注</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">粉丝</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">获赞</span>
+        </div>
+      </div>
+    </div>
+    <!-- grid导航 -->
+    <van-grid class="grid-nav" :column-num="2" clickable>
+      <van-grid-item class="grid-item">
+        <i slot="icon" class="toutiao toutiao-shoucang"></i>
+        <span slot="text" class="text">收藏</span>
+      </van-grid-item>
+      <van-grid-item class="grid-item">
+        <i slot="icon" class="toutiao toutiao-lishi"></i>
+        <span slot="text" class="text">历史</span>
+      </van-grid-item>
+    </van-grid>
+    <!-- 消息通知、小智同学、退出登录 -->
+    <van-cell title="消息通知" is-link />
+    <van-cell title="小智同学" class="md9" is-link />
+    <van-cell title="退出登录" class="lgout-cell" />
   </div>
 </template>
 
@@ -52,5 +97,75 @@ export default {
       }
     }
   }
+  // 已登录样式
+  .user-info {
+    .base-info {
+      height: 231px;
+      padding: 76px 32px 23px;
+      box-sizing: border-box;  // 将padding和boorder都计算在盒子内
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .left {
+        display: flex;
+        align-items: center;
+        .avatar {
+          width: 132px;
+          height: 132px;
+          margin-right: 23px;
+          border: 4px solid #fff;
+        }
+        .name {
+          font-size: 30px;
+          color: #fff;
+        }
+      }
+    }
+    .data-stats {
+      display: flex;
+      .data-item {
+        height: 130px;
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        color: #fff;
+        .count {
+          font-size: 36px;
+        }
+        .text {
+          font-size: 23px;
+        }
+      }
+    }
+  }
+
+  // grid-nav 历史收藏
+  .grid-nav {
+    .grid-item {
+      height: 141px;
+      i.toutiao {
+        font-size: 45px;
+      }
+      .toutiao-shoucang {
+        color: #eb5253;
+      }
+      .toutiao-lishi {
+        color: #ff9d1d;
+      }
+      span.text {
+        font-size: 28px;
+      }
+    }
+  }
+  .lgout-cell {
+    text-align: center;
+    color: #d86262;
+  }
+  .md9 {
+    margin-bottom: 9px;
+  }
+
 }
 </style>
