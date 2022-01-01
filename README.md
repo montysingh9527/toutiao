9546d6d0bdfd3663ae0b3e0c4b7ab7c543d8fc17
montysingh <montysingh9527@gmail.com>
2022-01-02 01:48:44 +0800

    首页-页面布局-头部导航栏搜索UI实现

--- README.md
+++ README.md
@@ -70,4 +70,20 @@ this.$toast.fail('登录失败,请稍后再试');
 this.$toast('登录成功');
 ```
 
-### 
+### 设置全局请求拦截器
+```
+// 添加请求拦截器
+request.interceptors.request.use((config) => {
+  // 请求发起会经过这里。 config:本次请求的配置对象
+  const { user } = store.state // Vuex中用户数据token
+  if (user && user.token) {
+    config.headers.Authorization=`Bearer ${user.token}`
+  }
+  // 注意：这里务必要返回config配置对象,否则请求就停在这里出不去了
+  return config;
+},(err) =>  {
+  // 如果请求出错(还没有发出去)会进入这里
+  console.log('请求拦截器拦截错误--->', err)
+  return Promise.reject(err);
+});
+```
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="home-container">首页</div>
+  <div class="home-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar">
+      <van-button class="search-btn" slot="title" type="info" size="small" icon="search" round>搜索</van-button>
+    </van-nav-bar>
+  </div>
 </template>
 
 <script>
@@ -18,4 +23,24 @@ export default {
 </script>
 
 <style scoped lang="less">
+.home-container {
+  // 要想改变组件自带的样式属性，添加 /deep/
+  /deep/.van-nav-bar__title {
+      max-width: unset;
+  }
+  .page-nav-bar{
+    background-color: #3296fa;
+  }
+  .search-btn {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+    border: none;
+    font-size: 28px;
+    .van-icon {
+      font-size: 32px;
+      color: #fff;
+    }
+  }
+}
 </style>
