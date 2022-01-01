296ca0406926f4df0e37671d285828516d536d64
montysingh <montysingh9527@gmail.com>
2022-01-02 00:59:07 +0800

    我的-设置请求拦截器，优化请求头设置

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,6 @@
 import request from '@/utils/request'
 // 使用 Vuex 必须先加载
-import store from '@/store'
+// import store from '@/store'
 
 // 登录
 export const login = data => {
@@ -31,9 +31,9 @@ export const login = data => {
     url: '/v1_0/user',
     // 发送请求头数据,该接口需要授权才能访问
     // 注意：token的数据格式：Bearer token数据, Bearer后面有个空格
-    headers: {
-      Authorization: `Bearer ${store.state.user.token}`
-    }
+    // headers: {
+    //   Authorization: `Bearer ${store.state.user.token}`
+    // }
   })
 }
 
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,27 @@
 import axios from 'axios' // 使用 Vuex 必须先加载
+import store from '@/store/index'
 
 // 再次创建一个实例 防止加载之前的会走拦截器
 // 用来回去新的 token
 const request = axios.create({
   baseURL: 'http://toutiao.itheima.net' // 接口的基准路径
 })
+
+// 添加请求拦截器
+request.interceptors.request.use((config) => {
+  // 请求发起会经过这里。 config:本次请求的配置对象
+  const { user } = store.state
+  if (user && user.token) {
+    config.headers.Authorization=`Bearer ${user.token}`
+  }
+  // 注意：这里务必要返回config配置对象,否则请求就停在这里出不去了
+  return config;
+},(err) =>  {
+  // 如果请求出错(还没有发出去)会进入这里
+  console.log('请求拦截器拦截错误--->', err)
+  return Promise.reject(err);
+});
+
 
 // 导出
 export default request
