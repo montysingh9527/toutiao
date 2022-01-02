0c089f01812fe988c09b359f2a0244561effca15
montysingh <montysingh9527@gmail.com>
2022-01-02 20:05:49 +0800

    首页-频道列表-展示文章列表,下拉刷新获取数据PullRefresh

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,14 +1,17 @@
 <template>
     <!-- 文章列表组件 -->
     <div class="article-list">
-        <!-- List列表组件：瀑布流滚动加载,用于展示长列表 -->
-        <van-list v-model="loading" :finished="finished"
-            finished-text="没有更多了"
-            :error.sync="error"
-            error-text="请求失败，点击重新加载"
-            @load="onLoad">
-            <van-cell v-for="(article, index) in list" :key="index" :title="article.title" />
-        </van-list>
+        <!-- 下拉刷新组件 PullRefresh  -->
+        <van-pull-refresh v-model="isFefreshLoading" :success-text="refreshSuccessText" :success-duration="1000" @refresh="onRefresh">
+            <!-- List列表组件：瀑布流滚动加载,用于展示长列表 -->
+            <van-list v-model="loading" :finished="finished"
+                finished-text="没有更多了"
+                :error.sync="error"
+                error-text="请求失败，点击重新加载"
+                @load="onLoad">
+                <van-cell v-for="(article, index) in list" :key="index" :title="article.title" />
+            </van-list>
+        </van-pull-refresh>        
     </div>
 </template>
 
@@ -29,7 +32,9 @@ props:{
     loading: false,  // 控制加载中 loading状态
     finished: false,  // 控制数据加载结束的状态
     timestamp: null,  // 请求获取下一页数据的时间戳
-    error: false    // 控制列表加载失败的提示状态
+    error: false,    // 控制列表加载失败的提示状态
+    isFefreshLoading: false,  // 控制下拉刷新的loading状态
+    refreshSuccessText: ''   // 下拉刷新成功提示文本
     }
   },
 computed:{},
@@ -49,7 +54,7 @@ methods: {
        const { results } = data.data
        // 数组的展开操作符,它会把数组元素一个一个拿出来
        this.list.push(...results)
-    //    console.log('results',data)       
+    //    console.log('results',data)  
        // 3、本次数据加载结束之后要把加载状态设置为结束,loading关闭以后才能触发下一次的加载更多
        this.loading = false
        // 4、判断数据是否全部加载完成。如果没有数据了,把finished设置为true,之后不再触发加载更多
@@ -65,6 +70,32 @@ methods: {
           this.loading = false   // 请求失败了,loading关闭      
       }
     },
+    // 当下拉刷新的时候会调用该函数
+    onRefresh(){
+        // 1、请求获取数据
+        getArticles({
+           channel_id: this.channel.id,   // 频道id
+           timestamp: Date.now(),  // 下拉刷新, 每次获取最新时间的数据
+           with_top: 1    // 是否包含置顶
+       }).then( res=> {
+            /**测试代码：模拟刷新失败的情况
+               if(Math.random() > 0.5){
+                  JSON.parse('results')
+            } */
+           // 2、将数据追加到列表的顶部
+           const { results } = res.data.data          
+            //console.log('results',results)
+           this.list.unshift(...results)
+           // 3、关闭下拉刷新的laoding状态
+            this.isFefreshLoading = false
+            // 更新下拉刷新成功提示的文本
+            this.refreshSuccessText = `刷新成功,更新了${results.length}条数据`
+       }).catch((err)=>{
+           this.refreshSuccessText = '刷新失败，请重试！'
+           this.isFefreshLoading = false
+       })  
+        
+    }
   }
 }
 </script>
