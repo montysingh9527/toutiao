c553e9c12e826e2f7ad71dfacd9a842b5f4dd2ef
montysingh <montysingh9527@gmail.com>
2022-01-02 14:17:17 +0800

    首页-页面布局-频道列表切换UI实现

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,6 +4,20 @@
     <van-nav-bar class="page-nav-bar">
       <van-button class="search-btn" slot="title" type="info" size="small" icon="search" round>搜索</van-button>
     </van-nav-bar>
+    
+    <!-- 频道列表 -->
+    <!-- 通过 animated 属性可以开启切换标签内容时的转场动画，通过 swipeable 属性可以开启滑动切换标签页。-->
+    <van-tabs class="channel-tabs" v-model="active" animated swipeable>
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <!-- 添加占位符充当内容区域：解决滑动到最后更多图标遮挡文字 -->
+      <div slot="nav-right" class="placeholder"></div>
+      <div slot="nav-right" class="hamburger-btn">
+        <i class="toutiao toutiao-gengduo"></i>
+      </div>
+    </van-tabs>
   </div>
 </template>
 
@@ -13,7 +27,9 @@ export default {
   components: {},
   props: {},
   data() {
-    return {};
+    return {
+      active: 0
+    };
   },
   computed: {},
   watch: {},
@@ -40,6 +56,59 @@ export default {
     .van-icon {
       font-size: 32px;
       color: #fff;
+    }
+  }
+  // /deep/ 深度操作符
+  /deep/ .channel-tabs {
+    .van-tabs_wrap {
+      height: 82px;
+    }
+    .van-tab {
+      width: 200px;
+      border-right: 1px solid #edeff3;
+      font-size: 30px;
+      color: #808080;
+    }
+    .van-tab--active {
+      color: 	#FF6347;
+    }
+    .van-tabs__line {
+      bottom: 8px;
+      width: 31px;
+      height: 6px;
+      background-color: #3296fa;;
+    }
+    .van-tabs__nav {
+      padding-bottom: 0;
+    }
+    .hamburger-btn {
+      position: fixed;
+      right: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 66px;
+      height: 82px;
+      background-color: #fff;
+      opacity: .9;
+      i.toutiao {
+        font-size: 33px;
+      }
+      &:before {
+        content: "";
+        position: absolute;
+        left: 0;
+        width: 2px;
+        height: 100%;
+        background-image: url(~@/assets/gradient-gray-line.png);
+        background-size: contain;
+      }
+    }
+    // 添加占位符充当内容区域：解决滑动到最后更多图标遮挡文字 
+    .placeholder {
+      flex-shrink: 0; // flex-shrink不参与flex布局计算
+      width: 66px;
+      height: 82px;
     }
   }
 }
